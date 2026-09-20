3defc4361b360201ab47609d5de63a0fe789fd69
double u <doubleyouuu09@gmail.com>
2026-09-20 20:05:17 +0700

    Tugas 1 - Latihan Pendekatan OOP dan Prosedural

---
+++ Tugas1/PendekatanProsedural.php
@@ -0,0 +1,13 @@
+<?php
+
+$panjang = 12;
+$lebar = 5;
+
+function hitungLuas($panjang, $lebar) {
+    return $panjang * $lebar;
+}
+
+$luas = hitungLuas ($panjang, $lebar);
+echo "Luas persegi panjang (Prosedural) = $luas";
+
+?>
