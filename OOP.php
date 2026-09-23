32c62912929024584646bb386d2e9ef559d00f81
double u <doubleyouuu09@gmail.com>
2026-09-23 17:28:26 +0700

    coba push

---
+++ OOP.php
@@ -0,0 +1,18 @@
+<?php
+
+class PersegiPanjang{
+    public function Luas() {
+        return $this->panjang * $this->lebar;
+    }
+}
+
+$perhitungan = new PersegiPanjang();
+$kotak->panjang = 10;
+$kotak->lebar = 5;
+
+$kotak2 = new PersegiPanjang();
+$kotak2->panjang = 15;
+$kotak2->lebar = 20;
+
+echo "Hasil 1 = "
+?>
